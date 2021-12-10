2befe57d7006498886b9158c7253ab2bd16ad082
Kim Nguyen <kimnivore@gmail.com>
2021-12-10 11:25:38 -0800

    names are on the page

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react';
+
+
+const Character = (props) => {
+    const {name} = props;
+
+    return (
+        <div>
+            <h2>{props.name}</h2>
+        </div>
+    );
+    }
+
+export default Character;
